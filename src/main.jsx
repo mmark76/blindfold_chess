9a4e6b1feda43b497bafa103762235c74ce2fe76
Markides.Markellos <markellos.markides@gmail.com>
2026-08-06 17:12:45 +0200

    feat: mount board visibility control

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import BoardVisibilityToggle from "./BoardVisibilityToggle.jsx";
 import ConfirmMoveButton from "./ConfirmMoveButton.jsx";
 import MovesVisibilityToggle from "./MovesVisibilityToggle.jsx";
 import "./styles.css";
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <App />
       <ConfirmMoveButton />
       <MovesVisibilityToggle />
+      <BoardVisibilityToggle />
     </>
   </React.StrictMode>
 );
